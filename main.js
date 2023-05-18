c14ef513d8363b2cb271584f41f7e8419f5bf496
jeremy-mazzocco <jeremyozzg@gmail.com>
2023-05-18 11:59:23 +0100

    ad title

---
+++ main.js
@@ -0,0 +1,14 @@
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            message: 'Ciao Ciao',
+
+        }
+    }
+}).mount('#app')
+
+
+// const { createApp } = Vue;
